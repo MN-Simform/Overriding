cccd3172abb664f527dd3eef196934832194599a
MN-Simform <nimit@simformsolutions.com>
2023-03-06 17:37:40 +0530

    Add Overriding Practical

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Pattern pattern = new Pattern();
+        Pattern patternM = new PatternM();
+        Pattern patternN = new PatternN();
+
+        pattern.display();
+        patternM.display();
+        patternN.display();
+    }
+}
